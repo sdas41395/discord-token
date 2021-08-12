aa1f79250fccc92931b4e54539157b6cfd50c7e7
Shoham Das <sdas41395@gmail.com>
2021-08-12 16:40:39 -0700

    asdfasd

--- bot_example.py
+++ bot_example.py
@@ -1,17 +1,39 @@
 import discord
+import json
+import random
+from discord.ext import commands
 
-client = discord.Client()
+token_dict = {}
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+bot = commands.Bot(command_prefix='/')
 
-@client.event
+@bot.command(name='list')
+async def _list(ctx):
+    for user in token_dict:
+        await ctx.send('User {} has {} coins'.format(user, token_dict[user]))
+
+@bot.event
+async def on_ready():   
+    print('We have logged in as {0.user}'.format(bot))
+
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    # Randomly award tokens
+    token_percent = random.randrange(1, 99)
+    if token_percent > 25:
+        token_amount = random.randrange(1, 99)
+        if str(message.author) not in token_dict:
+            token_dict[str(message.author)] = token_amount
+        else:
+            token_dict[str(message.author)] += token_amount
+        json_token = json.dumps(token_dict)
 
-client.run('your token here')
+    await bot.process_commands(message)
+    # await message.channel.send(f"You have been awarded {token_amount} tokens!")
+    # await message.channel.send(f"Current Ranking: {json_token}")
+    return
+
+bot.run('')
